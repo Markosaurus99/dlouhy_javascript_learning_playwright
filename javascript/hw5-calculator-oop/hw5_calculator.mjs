0b171be0e8a61c14e792c7b41c9774f227dc440a
Marek <marek.dlouhy99@gmail.com>
2025-03-22 14:30:57 +0100

    HW 5 calculator OOP

---
+++ javascript/hw5-calculator-oop/hw5_calculator.mjs
@@ -0,0 +1,8 @@
+import { Calculator } from "./hw5_calculator_blueprint.mjs";
+
+const calc = new Calculator();
+
+calc.add(14, 41);
+calc.subtract(174, 32);
+calc.multiply(47, 50);
+calc.divide(125, 0);
